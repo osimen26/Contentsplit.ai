08abf53fa9d93bd867adba19cfc643f4d92f3a29
OSIMEN <osimen30@gmail.com>
2026-04-24 15:43:17 +0100

    Fix Vercel API setup

--- api/index.js
+++ api/index.js
@@ -1,8 +1,16 @@
-import app from '../server/index.js'
+const http = require('http')
+const { createServer } = require('http')
 
-export default function handler(req, res) {
+// Import the Express app as a handler
+const appModule = require('../server/index.js')
+const app = appModule.default || appModule
+
+// Create a Vercel-compatible handler
+module.exports = async function handler(req, res) {
+  // Express app expects a req/res pair - wrap it
   return new Promise((resolve) => {
-    // Use express as request listener
-    app(req, res, resolve)
+    // Handle the request
+    app(req, res)
+    resolve()
   })
 }
