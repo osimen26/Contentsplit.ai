c81eb4f240621013412605029935e1346955278c
OSIMEN <osimen30@gmail.com>
2026-04-30 19:10:26 +0100

    Fix white screen: handle missing Google Client ID gracefully

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom/client'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 import App from './App'
 
-const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || 'missing_google_client_id_fallback'
+const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <GoogleOAuthProvider clientId={googleClientId}>
+const root = ReactDOM.createRoot(document.getElementById('root')!)
+
+if (!googleClientId || googleClientId === 'missing_google_client_id_fallback') {
+  console.error('VITE_GOOGLE_CLIENT_ID is not set! Google OAuth will be disabled.')
+  root.render(
+    <React.StrictMode>
       <App />
-    </GoogleOAuthProvider>
-  </React.StrictMode>,
-)
+    </React.StrictMode>
+  )
+} else {
+  root.render(
+    <React.StrictMode>
+      <GoogleOAuthProvider clientId={googleClientId}>
+        <App />
+      </GoogleOAuthProvider>
+    </React.StrictMode>
+  )
+}
